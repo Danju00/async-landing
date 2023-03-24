25dad57c13f181c822dd89ecbe5c4440409b4b64
Danju00 <leinad9265@gmail.com>
2023-03-24 00:32:47 -0600

    6.3 Desplegando el proyecto

--- src/assets/main.js
+++ src/assets/main.js
@@ -5,26 +5,26 @@ const content = null || document.getElementById('content');
 const options = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': 'fee3a65dffmsh1f92005892f66fap178085jsncbaa6c7faff9',
-		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+        'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+		'X-RapidAPI-Key': 'fee3a65dffmsh1f92005892f66fap178085jsncbaa6c7faff9'
 	}
 };
 
-async function fecthData(urlApi){
-    const response = await fecth(urlApi, options);
+async function fetchData(urlApi){
+    const response = await fetch(urlApi, options);
     const data = await response.json();
     return data;
 }
 
 (async () => {
     try{
-    const videos = await fecthData(API);
+    const videos = await fetchData(API);
     let view = `
     ${videos.items.map(video => ` 
         <div class="group relative">
                 <div
                     class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-                    <img src="${video.snippet.thumbnail.high.url}" alt="${video.snippet.description}" class="w-full">
+                    <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
                 </div>
                 <div class="mt-4 flex justify-between">
                     <h3 class="text-sm text-gray-700">
@@ -36,7 +36,8 @@ async function fecthData(urlApi){
     `).slice(0,4).join('')}
         
     `;
-    } catch{
-
+    content.innerHTML = view;
+    } catch (error){
+        console.log(error);
     }
 })();
